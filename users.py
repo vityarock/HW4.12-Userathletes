ef2d537caaa009cd701e7718937d8d41c56f970d
vityarock <57987552+vityarock@users.noreply.github.com>
2020-01-13 11:19:56 +0300

    Update users.py

--- users.py
+++ users.py
@@ -7,17 +7,16 @@ DB_PATH = "sqlite:///sochi_athletes.sqlite3"
 Base = declarative_base()
 
 class Users(Base):
-	"""Описывает структуру таблицы athletes для хранения записей"""
-	__tablename__ = "user"
-	id = sa.Column(sa.Integer, primary_key=True)
-	birthdate = sa.Column(sa.Text)
-	gender = sa.Column(sa.Text)
-	height = sa.Column(sa.REAL)
-	first_name = sa.Column(sa.Text)
-	last_name = sa.Column(sa.Text)
-	email = sa.Column(sa.Text)
-	def __str__(self):
-		return self.first_name
+    """Описывает структуру таблицы athletes для хранения записей"""
+    __tablename__ = "user"
+    id = sa.Column(sa.Integer, primary_key=True)
+    birthdate = sa.Column(sa.Text)
+    gender = sa.Column(sa.Text)
+    height = sa.Column(sa.REAL)
+    first_name = sa.Column(sa.Text)
+    last_name = sa.Column(sa.Text)
+    email = sa.Column(sa.Text)
+	
 	
 def request_data():
     """
@@ -45,14 +44,14 @@ def request_data():
     return user
 
 def main():
-	engine = sa.create_engine(DB_PATH)
-	Sessions = sessionmaker(engine)
-	session = Sessions()
-	user = request_data()
-	session.add(user)
-	session.commit()
-	print("Ваш ID: ", user.id)
+    engine = sa.create_engine(DB_PATH)
+    Sessions = sessionmaker(engine)
+    session = Sessions()
+    user = request_data()
+    session.add(user)
+    session.commit()
+    print("Ваш ID: ", user.id)
 
 
 if __name__ == "__main__":
-	main()
+    main()
